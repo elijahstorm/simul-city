422014fbd2302d1cbec9b4c2871789843ecc3d5d
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-01-25 18:18:23 -0600

    return 3d car ** broken
    This commit will not build, as the update migration steps have not yet been followed.

--- src/lib/Rendering/Car/FloatingCar.svelte
+++ src/lib/Rendering/Car/FloatingCar.svelte
@@ -2,6 +2,7 @@
 	import { Object3DInstance, Group, type Position, type Rotation } from '@threlte/core'
 	import { Collider } from '@threlte/rapier'
 	import { DEG2RAD } from 'three/src/math/MathUtils'
+	import { taxiCar, simpleCar } from '$lib/renders'
 	import FlyingControls from '../Bees/FlyingControls.svelte'
 	import type { Readable } from 'svelte/store'
 
@@ -10,3 +11,23 @@
 	export let movement: Readable<MovementController>
 	export let simple = true
 </script>
+
+{#if $simpleCar && $taxiCar}
+	<FlyingControls {position} {rotation} {movement} let:position let:rotation>
+		<Group {position} {rotation}>
+			<Collider mass={1} shape={'cuboid'} args={[1.25, 0.4, 0.5]} />
+
+			<!-- CAR BODY OBJECT -->
+			<Object3DInstance
+				castShadow
+				receiveShadow
+				object={simple ? $simpleCar : $taxiCar}
+				scale={0.005}
+				rotation={{ x: -90 * DEG2RAD, z: 90 * DEG2RAD }}
+				position={{ y: -0.6 }}
+			/>
+
+			<slot />
+		</Group>
+	</FlyingControls>
+{/if}
